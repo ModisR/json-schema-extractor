3b4bbb5a43d375deb11711a616715afc5890dcdb
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-08-14 16:11:16 +0100

    split up dependency graph logic

--- src/main/scala/ApiSpec.scala
+++ src/main/scala/ApiSpec.scala
@@ -1,11 +1,13 @@
 import play.api.libs.json.{JsObject, JsString, Reads, __}
 
+import scala.language.postfixOps
+
 final case class ApiSpec (schemas: Map[String, JsObject])
 
 object ApiSpec {
 
   implicit val reads: Reads[ApiSpec] =
-    (__ \ "components" \ "schemas").read[Map[String, JsObject]] map apply
+    schemaPath.read[Map[String, JsObject]] map apply
 
   def apply(schemas: Map[String, JsObject]): ApiSpec = applyImpl(Map.empty, schemas)
 
@@ -13,17 +15,25 @@ object ApiSpec {
     if (dec.isEmpty) {
       acc
     } else {
-      val dependencyRelations = dec.view.mapValues(
-        _ \\ "$ref" collect { case JsString(refPattern(dep)) => dep }
-      )
+      val schemaDependencyGraph = getSchemaDependencyGraphFrom(dec)
 
-      val dependencies = dependencyRelations.flatMap(_._2).toSet
-
-      val roots = dependencyRelations.keySet diff dependencies
+      val (_, roots) = partitionNonRootsOf(schemaDependencyGraph)
 
       dec.view.filterKeys(roots.contains).toMap
     }
   )
 
-  private lazy val refPattern = "#/components/schemas/([a-zA-Z0-9_]+)".r
+  private def partitionNonRootsOf(acyclicGraph: Map[String, Set[String]]) = {
+    val nonRoots = acyclicGraph.flatMap(_._2).toSet
+
+    acyclicGraph.keySet partition nonRoots.contains
+  }
+
+  private def getSchemaDependencyGraphFrom(schemas: Map[String, JsObject]) =
+    schemas.view.mapValues(
+      _ \\ "$ref" collect { case JsString(refPattern(dep)) => dep } toSet
+    ).toMap
+
+  private lazy val refPattern = s"#$schemaPath/([a-zA-Z0-9_]+)".r
+  private lazy val schemaPath = __ \ "components" \ "schemas"
 }
