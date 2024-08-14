a194421de408b0658d7fa533a688501a30436dab
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-08-14 18:30:37 +0100

    impl-ed dependency resolution; embedded required schemas in root schemas

--- src/main/scala/ApiSpec.scala
+++ src/main/scala/ApiSpec.scala
@@ -1,6 +1,6 @@
 import play.api.libs.json.{JsObject, JsString, Reads, __}
 
-import scala.language.postfixOps
+import scala.annotation.tailrec
 
 final case class ApiSpec (schemas: Map[String, JsObject])
 
@@ -9,31 +9,68 @@ object ApiSpec {
   implicit val reads: Reads[ApiSpec] =
     schemaPath.read[Map[String, JsObject]] map apply
 
-  def apply(schemas: Map[String, JsObject]): ApiSpec = applyImpl(Map.empty, schemas)
+  def apply(schemas: Map[String, JsObject]): ApiSpec = new ApiSpec(
+    {
+      val dependencyGraph = getDependencyGraphFrom(schemas)
+      val rootNodes = getRootNodesOf(dependencyGraph)
 
-  private def applyImpl(acc: Map[String, JsObject], dec: Map[String, JsObject]) = new ApiSpec(
-    if (dec.isEmpty) {
-      acc
-    } else {
-      val schemaDependencyGraph = getSchemaDependencyGraphFrom(dec)
+      def flatten(directedGraph: DirectedGraph) =
+        rootNodes
+          .map { rootNode =>
+            rootNode -> resolveDependenciesFor(rootNode, directedGraph)
+          }
+          .toMap
+          .withDefaultValue(Set.empty)
 
-      val (_, roots) = partitionNonRootsOf(schemaDependencyGraph)
+      val flattenedGraph = flatten(dependencyGraph)
 
-      dec.view.filterKeys(roots.contains).toMap
+      rootNodes
+        .map { node =>
+          val schema = schemas(node)
+          val dependencyNames = flattenedGraph(node)
+
+          val dependencySchemas = dependencyNames
+            .map(name => name -> schemas(name) )
+            .toMap
+
+          val dependencyJson = schemaPath.write[Map[String, JsObject]].writes(dependencySchemas)
+          node -> (schema ++ dependencyJson)
+        }
+        .toMap
     }
   )
 
-  private def partitionNonRootsOf(acyclicGraph: Map[String, Set[String]]) = {
-    val nonRoots = acyclicGraph.flatMap(_._2).toSet
 
-    acyclicGraph.keySet partition nonRoots.contains
+  private def resolveDependenciesFor(startNode: Node, directedGraph: DirectedGraph) = {
+    @tailrec
+    def impl(resolvedNodes: Set[Node], unresolvedNodes: Set[Node]): Set[Node] =
+      if(unresolvedNodes.isEmpty) resolvedNodes
+      else impl(
+        resolvedNodes union unresolvedNodes,
+        unresolvedNodes flatMap directedGraph diff resolvedNodes
+      )
+
+    impl(Set.empty, directedGraph(startNode))
   }
 
-  private def getSchemaDependencyGraphFrom(schemas: Map[String, JsObject]) =
-    schemas.view.mapValues(
-      _ \\ "$ref" collect { case JsString(refPattern(dep)) => dep } toSet
-    ).toMap
+  private def getRootNodesOf(directedGraph: DirectedGraph) = {
+    val nonRoots = directedGraph.flatMap(_._2).toSet
+
+    directedGraph.keySet diff nonRoots
+  }
+
+  private def getDependencyGraphFrom(schemas: Map[String, JsObject]) =
+    schemas.view
+      .mapValues( schema =>
+        (schema \\ "$ref")
+          .collect { case JsString(refPattern(dep)) => dep }
+          .toSet
+      )
+      .toMap
 
   private lazy val refPattern = s"#$schemaPath/([a-zA-Z0-9_]+)".r
   private lazy val schemaPath = __ \ "components" \ "schemas"
+
+  private type DirectedGraph = Map[Node, Set[Node]]
+  private type Node = String
 }
